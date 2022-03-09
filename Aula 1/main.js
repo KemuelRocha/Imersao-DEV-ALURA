3044f304ab0196d29f9004230006160e6a7815d0
Kemuel <kemuelsr@gmail.com>
2022-03-09 20:23:04 -0300

    Projetos Imersão ALURA

---
+++ Aula 1/main.js
@@ -0,0 +1,14 @@
+var nome = "Kemuel"
+
+var notaPrimeiroBimestre = 9
+var notaSegundoBimestre = 8
+var notaTerceiroBimestre = 5
+var notaQuartoBimestre = 2
+
+var notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(3)
+
+console.log("Bem vindo "+ nome)
+console.log(notaFixada)
+
---
+++ Aula 2/main.js
@@ -0,0 +1,11 @@
+function Converter(){
+    var valorElemento = document.getElementById("valor");
+    var valor = valorElemento.value;
+    var valorEmRealNumerico = parseFloat(valor);
+
+    var valorEmDolar = valorEmRealNumerico/5;
+
+    var elementoValorConvertido = document.getElementById("valorConvertido");
+    var valorConvertido  = "O resultado em Dólar é US$ " + valorEmDolar;
+    elementoValorConvertido.innerHTML = valorConvertido;
+}
