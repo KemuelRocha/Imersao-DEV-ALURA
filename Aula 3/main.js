1d5932d075eb393bd8d150569f408775a9af2ae4
Kemuel <kemuelsr@gmail.com>
2022-03-09 21:29:56 -0300

    Atualização

---
+++ Aula 3/main.js
@@ -0,0 +1,16 @@
+var numeroSecreto = parseInt(Math.random() * 11);
+
+function Chutar(){
+    var elementoResultado = document.getElementById("resultado");
+    var chute = parseInt(document.getElementById("valor").value);
+    console.log(chute);
+
+    if (chute == numeroSecreto){
+        elementoResultado.innerHTML = "Você acertou";
+    }else if(chute > 10 || chute < 0){
+        elementoResultado.innerHTML = "O numero deve estar entre 0 e 10";
+    }else{
+        elementoResultado.innerHTML = "Você errou, o numero secreto era: "+ numeroSecreto;
+    }
+
+}
